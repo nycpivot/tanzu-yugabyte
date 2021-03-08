da6333e7b1231492d085d5828bf19dace0e2cfaf
nycpivot <mijames@vmware.com>
2021-03-08 16:30:58 -0500

    Ongoing changes.

--- tanzu-yugabyte-workshop/scripts-download.sh
+++ tanzu-yugabyte-workshop/scripts-download.sh
@@ -1,7 +1,7 @@
 wget https://raw.githubusercontent.com/nycpivot/tanzu-yugabyte/main/tanzu-yugabyte-workshop/tanzu-kubernetes-grid/tkg-setup.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-yugabyte/main/tanzu-yugabyte-workshop/yugabyte-setup.sh
 
-mkdir yugabyte
+mkdir yugabyte-setup
 
-mv tkg-setup.sh yugabyte/tkg-setup.sh
-mv yugabyte-setup.sh yugabyte/yugabyte-setup.sh
+mv tkg-setup.sh yugabyte-setup/tkg-setup.sh
+mv yugabyte-setup.sh yugabyte-setup/yugabyte-setup.sh
