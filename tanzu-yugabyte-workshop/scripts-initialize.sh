10f70d0d28daa680bc33bdb25ae520ca006ddd68
nycpivot <mijames@vmware.com>
2021-03-08 16:23:55 -0500

    Ongoing changes.

--- tanzu-yugabyte-workshop/scripts-initialize.sh
+++ tanzu-yugabyte-workshop/scripts-initialize.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/nycpivot/tanzu-yugabyte/tanzu-yugabyte-workshop/scripts.sh
-sh scripts.sh
-rm scripts.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-yugabyte/tanzu-yugabyte-workshop/scripts-download.sh
+sh scripts-download.sh
+rm scripts-download.sh
