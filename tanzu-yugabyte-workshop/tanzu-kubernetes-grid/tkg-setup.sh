67868456d5bffae5e3425222eab580779632561c
nycpivot <mijames@vmware.com>
2021-03-08 17:59:32 -0500

    Ongoing changes.

--- tanzu-yugabyte-workshop/tanzu-kubernetes-grid/tkg-setup.sh
+++ tanzu-yugabyte-workshop/tanzu-kubernetes-grid/tkg-setup.sh
@@ -4,4 +4,4 @@ read -p "Worker Count: " worker_machine_count
 sudo tkg create cluster tanzu-yugabyte-${aws_config_name} --plan dev --worker-machine-count ${worker_machine_count} --config .tkg/${aws_config_name}.yaml
 sudo tkg get credentials tanzu-yugabyte-${aws_config_name} --config .tkg/${aws_config_name}.yaml
 
-sudo kubectl config use-context tanzu-yugabyte-${aws_config_name}-admin@tanzu-yugabyte-${aws_config_name}.yaml
+sudo kubectl config use-context tanzu-yugabyte-${aws_config_name}-admin@tanzu-yugabyte-${aws_config_name}
