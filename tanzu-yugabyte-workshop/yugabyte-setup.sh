e7c091feb4bbdda17dadab9d91dd5b023cb4ed88
nycpivot <mijames@vmware.com>
2021-03-08 17:21:01 -0500

    Ongoing changes.

--- tanzu-yugabyte-workshop/yugabyte-setup.sh
+++ tanzu-yugabyte-workshop/yugabyte-setup.sh
@@ -1,16 +1,21 @@
 wget https://tanzustorage.blob.core.windows.net/yugabyte/yugabyte-rbac.yaml
 wget https://tanzustorage.blob.core.windows.net/yugabyte/storage-class.yaml
-wget https://tanzustorage.blob.core.windows.net/yugabyte/generate-kubeconfig.py
 wget https://tanzustorage.blob.core.windows.net/yugabyte/yugabyte-k8s-secret.yaml
+wget https://tanzustorage.blob.core.windows.net/yugabyte/generate-kubeconfig.py
 
-sudo kubectl apply -f yugabyte-rbac.yaml
-sudo kubectl apply -f storage-class.yaml
+mv yugabyte-rbac.yaml yugabyte-setup/yugabyte-rbac.yaml
+mv storage-class.yaml yugabyte-setup/storage-class.yaml
+mv yugabyte-k8s-secret.yaml yugabyte-setup/yugabyte-k8s-secret.yaml
+mv generate-kubeconfig.py yugabyte-setup/generate-kubeconfig.py
 
-python generate-kubeconfig.py -s yugabyte-helm
+sudo kubectl apply -f yugabyte-setup/yugabyte-rbac.yaml
+sudo kubectl apply -f yugabyte-setup/storage-class.yaml
+
+python yugabyte-setup/generate-kubeconfig.py -s yugabyte-helm
 
 sudo kubectl create namespace yb-platform
 
-sudo kubectl create -f yugabyte-k8s-secret.yml -n yb-platform
+sudo kubectl create -f yugabyte-setup/yugabyte-k8s-secret.yml -n yb-platform
 
 sudo helm repo add yugabytedb https://charts.yugabyte.com
 sudo helm search repo yugabytedb/yugaware -l
