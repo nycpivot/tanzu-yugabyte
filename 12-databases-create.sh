16d007edb40166cf617ab256f54c04c076b4aab6
Michael James <mijames@vmware.com>
2022-03-31 20:49:28 -0400

    Refactored scripts.

--- 12-databases-create.sh
+++ 12-databases-create.sh
@@ -17,6 +17,10 @@ yugabyte-2.13.0.1/bin/ysqlsh -h $YB_TSERVER_URL -p 5433 -f tanzu-yugabyte/src/yu
 #BUILD POSTGRES SCHEMA (DOTNET - DATA IS WRITTEN BY APP)
 yugabyte-2.13.0.1/bin/ysqlsh -h $YB_TSERVER_URL -p 5433 -f tanzu-yugabyte/src/tanzu-yugabyte-dotnet/resources/schema.sql
 
+export JAVA_HOME=/opt/java/jdk-17
+export PATH=$PATH:/opt/java/jdk-17/bin
+export PATH=$PATH:/opt/maven/apache-maven-3.8.5/bin
+
 
 #WRITE TEST DATA INTO CASSANDRA
 cd tanzu-yugabyte/src/yugastore-java-master/resources
